2cf1bad8b1025e7ceeae747ac10b603a76044945
ThanoshanMV <mvthanoshan9@gmail.com>
2020-06-13 13:41:23 +0530

    Fixed adjMatrix length issue and adding more vertices, edges

--- Graphs/AdjacencyMatrix.java
+++ Graphs/AdjacencyMatrix.java
@@ -59,9 +59,14 @@ public class AdjacencyMatrix extends Graph{
         if(vertex >= adjMatrix.length){
             // Creating new 2D array with bigger size
             int[][] matrix = new int[vertex*2][vertex*2];
-            // Copying all the previous values from adjMatrix to new matrix
-            for (int i=0; i<matrix.length; i++){
-                for (int j=0; j<matrix.length; j++){
+            // Copying all the previous values from adjMatrix to new matrix.
+            /*
+            loop conditional check should be adjMatrix's length
+            because if you use new matrix's length there will be no entries for adjMatrix for the increased size
+            (It'll cause IndexOutOfBounds exception).
+             */
+            for (int i=0; i<adjMatrix.length; i++){
+                for (int j=0; j<adjMatrix.length; j++){
                     matrix[i][j] = adjMatrix[i][j];
                 }
             }
@@ -150,6 +155,15 @@ public class AdjacencyMatrix extends Graph{
         gh.addVertex(); // 1
         gh.addVertex(); // 2
         gh.addVertex(); // 3
+        gh.addVertex(); // 4
+        gh.addVertex(); // 5
+        gh.addVertex(); // 6
+        gh.addVertex(); // 7
+        gh.addVertex(); // 8
+        gh.addVertex(); // 9
+        gh.addVertex(); // 10
+        gh.addVertex(); // 11
+        gh.addVertex(); // 12
 
         System.out.println("Vertices Count in the Graph " + gh.getVertices());
         System.out.println("");
@@ -163,10 +177,27 @@ public class AdjacencyMatrix extends Graph{
         gh.addEdges(0,2);
         // 2 -> 3
         gh.addEdges(2,3);
+        // 3 -> 4
+        gh.addEdges(3,4);
+        // 3 -> 5
+        gh.addEdges(3, 5);
+        // 4 -> 6
+        gh.addEdges(4, 6);
+        // 4 -> 7
+        gh.addEdges(4, 7);
+        // 6 -> 8
+        gh.addEdges(6, 8);
+        // 5 -> 10
+        gh.addEdges(5,10);
+        // 5 -> 9
+        gh.addEdges(5, 9);
+        // 9 -> 11
+        gh.addEdges(9, 11);
+        // 9 -> 12
+        gh.addEdges(9, 12);
 
-        // Total Graph Representation:
-        // 0 -> 1
-        // 0 -> 2 -> 3
+        // Total Graph Representation: It's a directed graph.
+        // So try to draw them before running main method to see the output.
 
         // Checking out neighbors
         System.out.println("Vertex 0's out neighbour should be Vertex 1 and Vertex 2: " + gh.getNeighbors(0));
@@ -184,6 +215,17 @@ public class AdjacencyMatrix extends Graph{
 
         // Checking two hop neighbors
         System.out.println("Vertex 0's two hop neighbors should be Vertex 3: " + gh.getDistance2(0));
+        System.out.println("");
+
+        // Depth First Search
+        System.out.println("Path should be found: " + gh.depthFirstSearch(0,10));
+        System.out.println("Path should not be found: " + gh.depthFirstSearch(1,3));
+
+        System.out.println("");
+
+        // Breadth First Search
+        System.out.println("Path should be found: " + gh.breadthFirstSearch(0,10));
+        System.out.println("Path should not be found: " + gh.breadthFirstSearch(1,3));
 
 
     }
